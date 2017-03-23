91f545d67912c1aacae3762caf62b86a56af899c
javiergarciadiz <jagadi@gmail.com>
2017-03-23 12:47:56 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -12,8 +12,29 @@ body,td,th {
 </style></head>
 <body>           
      <h1>FDP | Planes de formación</h1>
-		<?
-			$link = new mysqli('173.194.85.219', 'root', 'Intelygenz01', 'np01_cursos');
+     
+<?php
+    //Función que nos retornara una conexión con mysqli
+    function crearConexion(){
+        //Datos para la conexión con el servidor
+        $servidor   = "173.194.85.219";
+        $nombreBD   = "np01_cursos";
+        $usuario    = "root";
+        $contrasena = "Intelygenz01";
+        //Creando la conexión, nuevo objeto mysqli
+        $conexion = new mysqli($servidor,$usuario,$contrasena,$nombreBD);
+        //Si sucede algún error la función muere e imprimir el error
+        if($conexion->connect_error){
+            die("Error en la conexion : ".$conexion->connect_errno.
+                                      "-".$conexion->connect_error);
+        }
+        //Si nada sucede retornamos la conexión
+        return $conexion;
+    }
+	
+?>     
+		<? /*
+			$link = new mysqli("173.194.85.219", "root", "Intelygenz01", "np01_cursos");
 //            $link = mysqli_connect("173.194.85.219","root","Intelygenz01","np01_cursos") or die ("No se ha podido conectar"); 
             mysqli_select_db($link,"np01_cursos") or die("Error al tratar de seleccionar esta base"); 
             $rstPlanes = mysqli_query("select nombre, marco_finalidades from np01_plan") or die ("Algo falló. 3");
@@ -24,7 +45,7 @@ body,td,th {
 			while ($filaPlanes = mysqli_fetch_array($rstPlanes)){
 				echo("<tr><td align=right nowrap>".$filaPlanes["nombre"]."</td><td align=right>".$filaPlanes["marco_finalidades"]."</td></tr>");
 				}	
-		?>
+*/		?>
 </table>
 </body>
 </html>
